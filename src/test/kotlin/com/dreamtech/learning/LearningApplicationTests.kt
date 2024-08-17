6600f12744fe1847fa1d03794d4f34208f6ad365
vijay <vijaypraneethn@gmail.com>
2024-08-17 23:37:55 +0530

    initializing spring boot project

---
+++ src/test/kotlin/com/dreamtech/learning/LearningApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dreamtech.learning
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
